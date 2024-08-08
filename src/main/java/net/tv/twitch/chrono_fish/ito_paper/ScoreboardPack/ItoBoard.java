81d45d4293481e41d9732325695ac4ac42e6ffb2
Daka-fish <yamatohidaka24@gmail.com>
2024-08-08 20:09:48 +0900

    edit scoreboard

--- src/main/java/net/tv/twitch/chrono_fish/ito_paper/ScoreboardPack/ItoBoard.java
+++ src/main/java/net/tv/twitch/chrono_fish/ito_paper/ScoreboardPack/ItoBoard.java
@@ -1,11 +1,9 @@
 package net.tv.twitch.chrono_fish.ito_paper.ScoreboardPack;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.tv.twitch.chrono_fish.ito_paper.GamePack.ItoPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.*;
 
 public class ItoBoard {
@@ -14,23 +12,37 @@ public class ItoBoard {
     private final Objective obj;
     private final ItoPlayer itoPlayer;
 
-    private final String yourNumber;
+    private String yourNumber;
 
     public ItoBoard(ItoPlayer itoPlayer){
         board = Bukkit.getScoreboardManager().getNewScoreboard();
         obj = board.registerNewObjective("sidebar", Criteria.DUMMY, Component.text("-Ito-").decorate(TextDecoration.BOLD));
         this.itoPlayer = itoPlayer;
-        yourNumber = "└ "+Component.text(String.valueOf(itoPlayer.getNumber())).color(TextColor.color(255,255,0));
+        yourNumber = "  └ §e"+itoPlayer.getNumber();
         obj.setDisplaySlot(DisplaySlot.SIDEBAR);
 
         Score s1 = obj.getScore("+あなたの数字:");
         Score s2 = obj.getScore("+宣言:");
+        Score s3 = obj.getScore("");
         Score number = obj.getScore(yourNumber);
 
         s1.setScore(-1);
         number.setScore(-2);
-        s2.setScore(-3);
+        s3.setScore(-3);
+        s2.setScore(-4);
+
+        for(int i = 1; i<10; i++){
+            Score entry = obj.getScore("  └"+i+":");
+            entry.setScore(-(i+4));
+        }
     }
 
     public Scoreboard getBoard(){return board;}
+
+    public void resetNumber(){
+        board.resetScores(yourNumber);
+        yourNumber = "   └ §e"+itoPlayer.getNumber();
+        obj.getScore(yourNumber).setScore(-2);
+
+    }
 }
